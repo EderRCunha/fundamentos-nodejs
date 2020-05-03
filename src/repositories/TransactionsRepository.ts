7fd84c75ea5b3d37f3eed011f4755935f66768ba
ederddsinfo <eder@ddsinfo.com.br>
2020-05-02 22:35:49 -0300

    fundamentosnodejs

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
-
+import Balance from '../models/Balance';
+/*
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+*/
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = new Balance({ income: 0, outcome: 0, total: 0 });
+
+    if (this.transactions) {
+      const incomes = this.transactions.filter(
+        transaction => transaction.type === 'income',
+      );
+
+      balance.income = incomes.reduce((sum, transaction) => {
+        return sum + transaction.value;
+      }, 0);
+
+      const outcomes = this.transactions.filter(
+        transaction => transaction.type === 'outcome',
+      );
+
+      balance.outcome = outcomes.reduce((sum, transaction) => {
+        return sum + transaction.value;
+      }, 0);
+
+      balance.total = balance.income - balance.outcome;
+    }
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
